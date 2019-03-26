3af93f7f168fd0b9b337e4784a245ebfa8c808b6
lvxiing <lxh1997zj@163.com>
2019-03-26 17:19:58 +0800

    the ninth commit

--- 剑指offer-牛客顺序/to_offer_36.py
+++ 剑指offer-牛客顺序/to_offer_36.py
@@ -1,51 +1,76 @@
 # !/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-'to_offer_35.py的归并解法，调试！'
+'输入两个链表，找出它们的第一个公共结点。'
 
-class Solution2:
-    def InversePairs(self, data):
+"""公共的节点，说明公共节点的值和next都是一样的，所以一定会有公共的尾部。"""
+
+"""
+①蛮力法：在第一个链表上顺序遍历每个节点，每遍历到一个节点，就在第二个链表上顺序遍历每个节点，直到找到第二个链表上有节点和第一个链表上的节点一样停止。（O(mn)）
+②如果从两个链表的尾部开始往前比较（后进先出），那么最后一个相同节点就是我们要找的节点，为此可以借助两个栈，栈顶元素都一样则pop出去，直到找到最后一个相同的元素为止。（时间复杂度O(m+n),且需要额外辅助空间）
+③首先遍历两个链表得到它们的长度，如果m>n，则m链表先走m-n步，然后两个链表再同时走，直到找到第一个相同的节点（即为它们的第一个公共节点）。（推荐，时间复杂度O(m+n),且不需要额外辅助空间）
+"""
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+class Solution:
+    def FindFirstCommonNode(self, pHead1, pHead2):  # 时间复杂度O(m+n), 但是不需要额外空间
         # write code here
-        if len(data) <= 0:
-            return 0
-        length = len(data)
-        copy = [0] * length
-        for i in range(length):
-            copy[i] = data[i]
-        count = self.Core(data, copy, 0, length-1)
-        return count % 1000000007
+        if not pHead1 or not pHead2:
+            return None
+        length1 = self.HeadLength(pHead1)
+        length2 = self.HeadLength(pHead2)
+        if length1 > length2:
+            headLong = pHead1
+            headShort = pHead2
+        else:
+            headLong = pHead2
+            headShort = pHead1
+        diff = abs(length1 - length2)
+        for i in range(diff):
+            headLong = headLong.next
+        while headLong != None and headShort != None and headLong != headShort:
+            headLong = headLong.next
+            headShort = headShort.next
+        return headLong
 
-    def Core(self, data, copy, start, end):
-        if start == end:
-            copy[start] = data[start]
-            return 0
-        length = (end - start) // 2
-        left = self.Core(copy, data, start, start+length)
-        right = self.Core(copy, data, start+length+1, end)
-        i = start + length
-        j = end
-        copyindex = end
-        count = 0
-        while i >= start and j >= start+length+1:
-            if data[i] > data[j]:
-                copy[copyindex] = data[i]
-                copyindex -= 1
-                i -= 1
-                count += j - start - length
+    def HeadLength(self, pHead):
+        length = 0
+        while pHead:
+            pHead = pHead.next
+            length += 1
+        return length
+
+
+class Solution1:
+    def FindFirstCommonNode(self, pHead1, pHead2):  # 时间复杂度O(m+n), 但是需要额外空间
+        # write code here
+        if not pHead1 or not pHead2:
+            return None
+        stack1, stack2 = [], []
+        while pHead1:
+            stack1.append(pHead1)
+            pHead1 = pHead1.next
+        while pHead2:
+            stack2.append(pHead2)
+            pHead2 = pHead2.next
+        first = None                # 这里可以在设置一个数组来存放相同的结点，最后一个相同的结点就是第一个公共结点
+        while stack1 and stack2:
+            node1 = stack1.pop()
+            node2 = stack2.pop()
+            if node1 == node2:
+                first = node1
             else:
-                copy[copyindex] = data[j]
-                copyindex -= 1
-                j -= 1
-        while i >= start:
-            copy[copyindex] = data[i]
-            copyindex -= 1
-            i -= 1
-        while j >= start+length+1:
-            copy[copyindex] = data[j]
-            copyindex -= 1
-            j -= 1
-        return count + left + right
+                break
+        return first
 
-# 测试:
-s = Solution2()
-print(s.InversePairs([1, 2, 3, 4, 5, 6, 7, 0]))
+        # 另外在开辟一个新数组存放相同的结点:
+        # res = []
+        # if node1 == node2:
+        #     res.append(node1)
+        # if res:
+        #     node = res.pop()
+        # return node
--- 剑指offer-牛客顺序/to_offer_37.py
+++ 剑指offer-牛客顺序/to_offer_37.py
@@ -1,64 +1,59 @@
 # !/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-'在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组,求出这个数组中的逆序对的总数P。并将P对1000000007取模的结果输出。 即输出P%1000000007'
+'统计一个数字在排序数组中出现的次数。'
 
-class Solution2:
-    def InversePairs(self, data):
-        if len(data) <= 0:
-            return 0
-        length = len(data)
-        copy = [0] * length
-        for i in range(length):
-            copy[i] = data[i]
-        # copy数组为原数组data的复制,在后面充当辅助数组
-        count = self.Core(data, copy, 0, length - 1)
-        return count % 1000000007
+"""
+思路：二分查找法，分别找到此数字在排序数组中第一次和最后一次出现的位置，然后次数等于两个位置之差加1。
+时间复杂度：O(log n)
+"""
 
 
-    def Core(self, data, copy, start, end):
-        if start == end:
-            copy[start] = data[start]
-            return 0
+class Solution:
+    def GetNumberOfK(self, data, k):
+        # write code here
+        number = 0
+        if data != None and len(data) > 0:
+            length = len(data)
+            first = self.FirstNumber(data, length, k, 0, length-1)
+            last = self.LastNumber(data, length, k, 0, length-1)
+            if first > -1 and last > -1:
+                number = last - first + 1
+        return number
 
-        length = (end - start) // 2  # length为划分后子数组的长度
-
-        left = self.Core(copy, data, start, start + length)
-        right = self.Core(copy, data, start + length + 1, end)
-
-        # 初始化i为前半段最后一个数字的下标
-        i = start + length
-        # 初始化j为后半段最后一个数字的下标
-        j = end
-
-        # indexCopy为辅助数组的指针，初始化其指向最后一位
-        indexCopy = end
-        # 准备开始计数
-        count = 0
-        # 对两个数组进行对比取值的操作：
-        while i >= start and j >= start + length + 1:
-            if data[i] > data[j]:
-                copy[indexCopy] = data[i]
-                indexCopy -= 1
-                i -= 1
-                count += j - start - length
+    def FirstNumber(self, data, length, k, start, end):
+        if start > end:
+            return -1
+        middle = (start+end) // 2
+        if data[middle] == k:
+            if middle > 0 and data[middle-1] == k:
+                end = middle - 1
             else:
-                copy[indexCopy] = data[j]
-                indexCopy -= 1
-                j -= 1
+                return middle
+        elif data[middle] > k:
+            end = middle - 1
+        else:
+            start = middle + 1
+        return self.FirstNumber(data, length, k, start, end)
 
-        # 剩下一个数组未取完的操作：
-        while i >= start:
-            copy[indexCopy] = data[i]
-            indexCopy -= 1
-            i -= 1
-        while j >= start + length + 1:
-            copy[indexCopy] = data[j]
-            indexCopy -= 1
-            j -= 1
-
-        return count + left + right
+    def LastNumber(self, data, length, k, start, end):
+        if start > end:
+            return -1
+        middle = (start+end) // 2
+        if data[middle] == k:
+            if middle < end and data[middle+1] == k:
+                start = middle + 1
+            else:
+                return middle
+        elif data[middle] > k:
+            end = middle - 1
+        else:
+            start = middle + 1
+        return self.LastNumber(data, length, k, start, end)
 
 # 测试:
-s = Solution2()
-print(s.InversePairs([1, 2, 3, 4, 5, 6, 7, 0]))
+alist = [3, 3, 3, 3, 4, 5]
+s = Solution()
+print(s.GetNumberOfK(alist, 3))
+print(s.GetNumberOfK(alist, 4))
+print(s.GetNumberOfK(alist, 5))
