48d8d4d802526f2ecc5500eba488d101d590f15b
lvxiing <lxh1997zj@163.com>
2019-04-01 17:40:30 +0800

    Fifteenth submission

--- 剑指offer-牛客顺序/to_offer_46.py
+++ 剑指offer-牛客顺序/to_offer_46.py
@@ -1,2 +1,70 @@
 # !/usr/bin/env python3
 # -*- coding:utf-8 -*-
+
+'题目：圆圈中最后剩下的数字'
+
+"""题：0，1，...，n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字。求出这个圆圈里剩下的最后一个数字."""
+
+"""
+解题思路：
+约瑟夫环问题，可以根据数学规律找出高效的解法，剑指offer-P302.
+公式: f(n, m) = [f(n-1, m) + m] % n  (n > 1)
+                0                   (n = 1)
+链接2: https://blog.csdn.net/u012505432/article/details/51747181
+"""
+
+
+class Solution:
+    def LastRemaining_Solution(self, n, m):
+        # write code here
+        if n < 1 or m < 1:
+            return -1
+        res = 0
+        for i in range(2, n+1):  # 这里循环从1和2开始没什么区别
+            res = (res + m) % i
+        return res
+
+
+# 测试:
+s = Solution()
+print(s.LastRemaining_Solution(5, 3))
+print(s.LastRemaining_Solution(6, 2))
+print(s.LastRemaining_Solution(5, 6))
+print(s.LastRemaining_Solution(6, 6))
+print(s.LastRemaining_Solution(0, 2))
+print(s.LastRemaining_Solution(4000, 997))
+print('----------------------------------')
+
+
+class Solution1:
+    def LastRemaining_Solution(self, n, m): # 这个存在语法错误或者数组越界非法访问等情况，通不过oj
+        # write code here
+        if n < 1 or m < 1:
+            return -1
+        cnt = 1
+        dead = []
+        def ans(t, m):
+            nonlocal cnt, dead
+            for i in range(t):
+                if i in dead:
+                    continue
+                if cnt == m:
+                    dead.append(i)
+                    cnt = 1
+                else:
+                    cnt += 1
+        while len(dead) < n-1:
+            ans(n, m)
+        for i in range(n):
+            if i not in dead:
+                return i
+
+
+# 测试:
+s = Solution1()
+print(s.LastRemaining_Solution(5, 3))
+print(s.LastRemaining_Solution(6, 2))
+print(s.LastRemaining_Solution(5, 6))
+print(s.LastRemaining_Solution(6, 6))
+print(s.LastRemaining_Solution(0, 2))
+# print(s.LastRemaining_Solution(4000, 997)) # 这种情况不能正常输出
--- 剑指offer-牛客顺序/to_offer_47.py
+++ 剑指offer-牛客顺序/to_offer_47.py
@@ -1,2 +1,48 @@
 # !/usr/bin/env python3
 # -*- coding:utf-8 -*-
+
+'求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。'
+
+class Solution:
+    def Sum_Solution(self, n):
+        # write code here
+        return n and self.Sum_Solution(n-1) + n  # 终止递归采用逻辑与的短路特性
+
+
+class Solution1:
+    def Sum_Solution(self, n):
+        return sum(list(range(1, n+1)))   # Python自带的sum函数
+
+
+"""
+思路二：利用两个函数，一个函数充当递归函数的角色，另一个函数处理终止递归的情况。如果对n连续进行两次反运算，
+那么非零的n转换为True，0转换为False。利用这一特性终止递归。注意考虑测试用例为0的情况。
+"""
+
+
+class Solution2:
+    def Sum_Solution(self, n):
+        return self.sumN(n)
+
+    def sum0(self, n):
+        return 0
+
+    # 利用非0值作两次非运算返回false, 0作两次非运算返回True
+    def sumN(self, n):
+        func = {False: self.sum0, True: self.sumN}
+        # 此处的fun[not not n] 不能写作func[not not n-1], 否则测试用例为0的话, 就会无限次迭代
+        return n + func[not not n](n - 1)
+
+
+# 测试:
+s = Solution()
+print(s.Sum_Solution(5))
+print(s.Sum_Solution(0))
+print('---------------')
+s = Solution1()
+print(s.Sum_Solution(5))
+print(s.Sum_Solution(0))
+print('---------------')
+s = Solution2()
+print(s.Sum_Solution(5))
+print(s.Sum_Solution(0))
